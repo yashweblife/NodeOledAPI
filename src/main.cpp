eafccc62456f4942c896e7272f4e20b0bdd9a798
yashweblife <yashweblife20@gmail.com>
2023-08-04 23:28:16 -0500

    Add server endpoints

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,19 @@ IPAddress local_IP(192, 168, 1, 184);
 IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 255, 0);
 
+ESP8266WebServer server(80);
+void handleRoot(){
+  server.send(200, "text/plain", "Connected");
+  display.clear();
+  display.drawString(0, 0, "Client Connected");
+  display.display();
+}
+void handleNotFound(){
+  server.send(404, "text/plain", "Not Found");
+  display.clear();
+  display.drawString(0, 0, "Bad EndPoint");
+  display.display();
+}
 void setup() {
   display.init();
   display.clear();
@@ -32,9 +45,40 @@ void setup() {
   display.drawString(0, 16, "IP: ");
   display.drawString(0, 32, WiFi.localIP().toString());
   display.display();
+
+  server.on("/",handleRoot);
+  server.onNotFound(handleNotFound);
+
+  server.on("/msg",[](){
+    server.send(200, "text/plain", "Message Received");
+    String value = server.arg("msg");
+    int x = server.arg("x").toInt();
+    int y = server.arg("y").toInt();
+    bool clear = server.arg("clear").toInt();
+    if(clear){
+      display.clear();
+    }
+    display.drawString(x,y,value);
+    display.display();
+  });
+  server.on("/box",[](){
+    server.send(200, "text/plain", "Box Received");
+    int w = server.arg("w").toInt();
+    int h = server.arg("h").toInt();
+    int x = server.arg("x").toInt();
+    int y = server.arg("y").toInt();
+    display.drawRect(x,y,w,h);
+    display.display();
+  });
+  server.on("/clear",[](){
+    server.send(200, "text/plain", "Cleared");
+    display.clear();
+    display.display();
+  });
+  server.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  server.handleClient();
 }
 
