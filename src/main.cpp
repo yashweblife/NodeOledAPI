73139369bb793c7ea86087e5a6706dc14b89c711
yashweblife <yashweblife20@gmail.com>
2023-08-05 05:33:29 -0500

    even more functions

--- src/main.cpp
+++ src/main.cpp
@@ -25,10 +25,11 @@ void handleNotFound(){
 void setup() {
   display.init();
   display.clear();
-
+  pinMode(12,OUTPUT);
+  pinMode(14,OUTPUT);
   WiFi.mode(WIFI_STA);
   WiFi.config(local_IP, gateway, subnet);
-  WiFi.begin(SSID, PSWD);
+  WiFi.begin(SID, PSWD);
   int wifiLoader = 0;
   while(WiFi.status() != WL_CONNECTED){
     if(wifiLoader >100){
@@ -48,7 +49,22 @@ void setup() {
 
   server.on("/",handleRoot);
   server.onNotFound(handleNotFound);
-
+  server.on("/rssi",[](){
+    server.send(200, "text/plain", String(WiFi.RSSI()));
+  });
+  server.on("/networks",[](){
+    //send all wifi network names as json array
+    int n = WiFi.scanNetworks();
+    String networks = "[";
+    for(int i = 0; i < n; i++){
+      networks += "\"" + WiFi.SSID(i) + "\"";
+      if(i < n-1){
+        networks += ",";
+      }
+    }
+    networks += "]";
+    server.send(200, "text/plain", networks);
+  });
   server.on("/msg",[](){
     server.send(200, "text/plain", "Message Received");
     String value = server.arg("msg");
@@ -67,14 +83,70 @@ void setup() {
     int h = server.arg("h").toInt();
     int x = server.arg("x").toInt();
     int y = server.arg("y").toInt();
+    bool clear = server.arg("clear").toInt();
+    if(clear){
+      display.clear();
+    }
     display.drawRect(x,y,w,h);
     display.display();
   });
+  server.on("/fillbox",[](){
+    server.send(200, "text/plain", "Fill Box Received");
+    int w = server.arg("w").toInt();
+    int h = server.arg("h").toInt();
+    int x = server.arg("x").toInt();
+    int y = server.arg("y").toInt();
+    bool clear = server.arg("clear").toInt();
+    if(clear){
+      display.clear();
+    }
+    display.fillRect(x,y,w,h);
+    display.display();
+  });
+  server.on("/line",[](){
+    server.send(200, "text/plain", "Line Received");
+    int x1 = server.arg("x1").toInt();
+    int y1 = server.arg("y1").toInt();
+    int x2 = server.arg("x2").toInt();
+    int y2 = server.arg("y2").toInt();
+    bool clear = server.arg("clear").toInt();
+    if(clear){
+      display.clear();
+    }
+    display.drawLine(x1,y1,x2,y2);
+    display.display();
+  });
+  server.on("/circle",[](){
+    server.send(200, "text/plain", "Circle Received");
+    int x = server.arg("x").toInt();
+    int y = server.arg("y").toInt();
+    int r = server.arg("r").toInt();
+    bool clear = server.arg("clear").toInt();
+    if(clear){
+      display.clear();
+    }
+    display.drawCircle(x,y,r);
+    display.display();
+  });
+  server.on("/d1",[](){
+    digitalWrite(12,1);
+    int value = analogRead(A0);
+    digitalWrite(12,0);
+    server.send(200, "text/plain", String(value));
+  });
+  server.on("/d2",[](){
+    digitalWrite(14,1);
+    int value = analogRead(A0);
+    digitalWrite(14,0);
+    server.send(200, "text/plain", String(value));
+  });
+
   server.on("/clear",[](){
     server.send(200, "text/plain", "Cleared");
     display.clear();
     display.display();
   });
+  server.enableCORS(true);
   server.begin();
 }
 
